52a6413004366a6f478eeb455b149e7676e45059
yourGithubUsername <your_github_email@example.com>
2017-09-14 12:29:25 -0400

    nav and user

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import User from "./components/User";
+import GlobalNav from "./components/GlobalNav";
+
+
 
 class App extends Component {
+    
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      
+         <Router>
+        <div>
+          <GlobalNav />
+          <Route path="/" component={HomePage}/>
+          <Route path="/user/:id" component={User}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
+
 
 export default App;
---
+++ client/src/components/User.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+
+class User extends Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello from User</h1>
+      </div>
+    )
+  }
+}
+
+
+export default User;
